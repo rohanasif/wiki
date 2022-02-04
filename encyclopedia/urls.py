6d665c71dc3c621fbf80fbce0281bdc355f75824
Rohan <rohanasif@yahoo.com>
2022-02-04 23:48:26 +0500

    inputting the entry in url displays the page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/css", views.css, name="css"),
+    path("wiki/django", views.django, name="django"),
+    path("wiki/git", views.git, name="git"),
+    path("wiki/html", views.html, name="html"),
+    path("wiki/python", views.python, name="python")
 ]
