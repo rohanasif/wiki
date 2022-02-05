6273b61d0b509838817ba005431d024d7a0c1f9c
Rohan <rohanasif@yahoo.com>
2022-02-05 19:45:39 +0500

    Solved

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/css", views.css, name="css"),
-    path("wiki/django", views.django, name="django"),
-    path("wiki/git", views.git, name="git"),
-    path("wiki/html", views.html, name="html"),
-    path("wiki/python", views.python, name="python")
+    path("edit/<str:title>", views.edit, name="edit"),
+    path("create", views.create, name="create"),
+    path("search", views.search, name="search"),
+    path("randoms", views.randoms, name="randoms"),
+    path("<str:title>", views.entry, name="entry")
 ]
